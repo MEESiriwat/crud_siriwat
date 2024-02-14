aa81ad160521c2b61462bf5ad6b1c234980d8195
SIRIWAT 3 <136049088+MEESiriwat@users.noreply.github.com>
2024-02-14 11:06:51 +0700

    Add files via upload

---
+++ CRUD_SIRIWAT/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <?php require_once "connect.php"?>
+    <?php require_once "sethead.php"?>
+</head>
+<body>
+    <h1 class="container mt-5">ศิริวรรษ หล่อมาก</h1>
+</body>
+    <?php require_once "setfoot.php"?>
+</html>
